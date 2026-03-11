c65bd2bcaca02f657bac6d6ba2c95985a47f9029
Lance Thongsavanh <lancer8106@gmail.com>
2026-03-11 12:53:46 -0400

    Added documentation for the Die class. Fixed the roll_dice function so that it now correctly generates a number between 1 and the total amount of sides the Die has.

--- Unit-9-Participation.py
+++ Unit-9-Participation.py
@@ -6,12 +6,24 @@ Make a 10-sided die and a 20-sided die. Roll each die 10 times.
 """
 import random
 class Die:
+    """This class represents a singular die
+    """
 
     def __init__(self, __sides: int = 6) -> None:
+        """This initializes the Die object and creates and sets the __sides attribute
+
+        Args:
+            __sides (int, optional): Determines the number of sides the Die will have. Defaults to 6.
+        """
         self.__sides: int = __sides
     
     def roll_dice(self) -> int:
-        return random.randint(0,self.__sides)
+        """Generates a random number between 1 and how many sides the Die has and returns that number
+
+        Returns:
+            int: the random number between 1 and the number of sides
+        """
+        return random.randint(1,self.__sides)
     
 die_6: Die = Die()
 print("Rolling 6-sided die 10 times")
