69cfe346c9788e1259257e5b00c0dd9073e3a78f
Lance Thongsavanh <lancer8106@gmail.com>
2026-03-11 12:47:40 -0400

    Created the Die class with the __init__ and roll_dice functions. Created a die object with 6-sides and called the roll_dice funtion 10 times using a 'for' loop.

--- Unit-9-Participation.py
+++ Unit-9-Participation.py
@@ -1,5 +1,19 @@
 """
-9-13. Dice: Make a class Die with one attribute called sides, which has a default value of 6. Write a method called roll_die() that prints a random number between 1 and the number of sides the die has. Make a 6-sided die and roll it 10 times.
+9-13. Dice: Make a class Die with one attribute called sides, which has a default value of 6. Write a method called roll_die() that prints a random number between 1 and the number of sides the die has. 
 
+Make a 6-sided die and roll it 10 times.
 Make a 10-sided die and a 20-sided die. Roll each die 10 times.
 """
+import random
+class Die:
+
+    def __init__(self, __sides: int = 6) -> None:
+        self.__sides: int = __sides
+    
+    def roll_dice(self) -> int:
+        return random.randint(0,self.__sides)
+    
+die_6: Die = Die()
+print("Rolling 6-sided die 10 times")
+for i in range(10):
+    print(f"{die_6.roll_dice()}")
